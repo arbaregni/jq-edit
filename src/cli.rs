dfce5b4d7f142790f12a982f801004efadec5495
Valorie Sours <jamesnsours@gmail.com>
2024-08-23 20:16:28 -0400

    turn off logging by default

--- src/cli.rs
+++ src/cli.rs
@@ -4,5 +4,6 @@ use clap::Parser;
 #[command(version, about)]
 pub struct Cli {
     #[arg(long, default_value_t = log::LevelFilter::Off)]
+    /// The level to log at.
     pub level_filter: log::LevelFilter
 }
