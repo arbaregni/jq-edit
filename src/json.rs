6462a7bd1be1e42051a50ee40e0278f95c29c497
Valorie Sours <jamesnsours@gmail.com>
2024-08-26 20:09:08 -0400

    WIP for custom json parsing

--- src/json.rs
+++ src/json.rs
@@ -17,7 +17,7 @@ pub struct JsonDataInner<'a> {
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum JsonDataType<'a> {
-    Object { elems: Vec<(JsonKey<'a>, JsonData<'a>)> },
+    Object { entries: Vec<(JsonKey<'a>, JsonData<'a>)> },
     Array { elems: Vec<JsonData<'a>> },
     Str,
     Boolean,
@@ -31,12 +31,20 @@ pub struct JsonKey<'a> {
 
 
 struct ParsingContext<'a> {
-    source: &'a str,
+    _source: &'a str,
     tokens: Vec<Token<'a>>,
     idx: usize,
     errs: Vec<String>,
 }
 impl <'a> ParsingContext<'a> {
+    fn from(source: &'a str, tokens: Vec<Token<'a>>) -> Self {
+        Self {
+            _source: source,
+            tokens,
+            idx: 0,
+            errs: vec![],
+        }
+    }
     fn peek(&self) -> Token<'a> {
         match self.tokens.get(self.idx) {
             Some(tok) => *tok,
@@ -46,22 +54,20 @@ impl <'a> ParsingContext<'a> {
             }
         }
     }
-    fn matches(&self, tty: TokenType) -> bool {
-        self.peek().tty == tty
-    }
     fn eat_whitespace(&mut self) {
         // we can ignore white space while parsing
         while self.peek().tty.is_whitespace() {
             self.idx += 1;
         }
     }
-    fn consume(&mut self, tty: TokenType) -> Result<()> {
+    fn consume(&mut self, tty: TokenType) -> Result<Token<'a>> {
         self.eat_whitespace();
-        if !self.matches(tty) {
+        let tok = self.peek();
+        if tok.tty != tty {
             bail!("Expected {tty:?}, got {:?}", self.peek().tty);
         }
         self.idx += 1;
-        Ok(())
+        Ok(tok)
     }
     fn parse_json(&mut self) -> Result<JsonData<'a>> {
         self.eat_whitespace();
@@ -70,7 +76,7 @@ impl <'a> ParsingContext<'a> {
         let json = match tty {
             TokenType::OpenBrace => self.parse_object()?,
             TokenType::OpenBracket => self.parse_array()?,
-            TokenType::String => self.parse_single_token_as(JsonDataType::Str)?,
+            TokenType::String => self.parse_string()?,
             TokenType::Number => self.parse_single_token_as(JsonDataType::Number)?,
             TokenType::Boolean => self.parse_single_token_as(JsonDataType::Boolean)?,
             _ => { 
@@ -80,7 +86,43 @@ impl <'a> ParsingContext<'a> {
         Ok(json)
     }
     fn parse_object(&mut self) -> Result<JsonData<'a>> {
-        bail!("todo");
+        self.consume(TokenType::OpenBrace)?;
+
+        let mut entries = Vec::new();
+
+        loop {
+            let key = self.parse_string()?;
+            let _ = self.consume(TokenType::Colon)?;
+            let value = self.parse_json()?;
+
+            let key = JsonKey {
+                lex: key.0.lex
+            };
+
+            entries.push((key, value));
+
+            match self.peek().tty {
+                TokenType::Comma => {
+                    self.consume(TokenType::Comma)?;
+                    // go around again (no trailing comma allowed !)
+                }
+                TokenType::CloseBrace => {
+                    self.consume(TokenType::CloseBrace)?;
+                    break;
+                }
+                _ => {
+                    bail!("unexpected token while parsing object: {:?}", self.peek().tty);
+                }
+            }
+        }
+
+        Ok(JsonData(JsonDataInner {
+            ty: JsonDataType::Object {
+                entries
+            },
+            lex: Cow::from("<todo>"),
+        }))
+
     }
     fn parse_array(&mut self) -> Result<JsonData<'a>> {
         self.consume(TokenType::OpenBracket)?;
@@ -102,6 +144,7 @@ impl <'a> ParsingContext<'a> {
             match self.peek().tty {
                 TokenType::Comma => {
                     // go around again (this is no trailing comma parsing)
+                    self.consume(TokenType::Comma)?;
                 }
                 TokenType::CloseBracket => {
                     // consume it and break
@@ -119,6 +162,16 @@ impl <'a> ParsingContext<'a> {
             lex: Cow::from("todo"),
        }))
     }
+    fn parse_string(&mut self) -> Result<JsonData<'a>> {
+        self.eat_whitespace();
+
+        let tok = self.consume(TokenType::String)?;
+        let lex = Cow::Borrowed(tok.lex);
+        Ok(JsonData(JsonDataInner {
+            ty: JsonDataType::Str,
+            lex
+        }))
+    }
     fn parse_single_token_as(&mut self, ty: JsonDataType<'a>) -> Result<JsonData<'a>> {
         self.eat_whitespace();
 
@@ -133,20 +186,56 @@ impl <'a> ParsingContext<'a> {
 
 pub fn loads<'a>(source: &'a str) -> Result<JsonData<'a>> {
     let tokens = tokens::tokenize(source);
-    let mut ctx = ParsingContext {
-        source,
-        tokens,
-        idx: 0,
-        errs: vec![],
-    };
+    let mut ctx = ParsingContext::from(source, tokens);
     let json = ctx.parse_json()?;
     ctx.eat_whitespace();
     ctx.consume(TokenType::Eof)?;
     Ok(json)
 }
 
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    #[test]
+    fn test_peek() {
+        let source = "blah";
+        let tokens = vec![
+            Token {
+                tty: TokenType::Number,
+                lex: "34",
+            },
+            Token {
+                tty: TokenType::String,
+                lex: "\"hello\""
+            }
+        ];
+        let ctx = ParsingContext::from(source, tokens); 
+
+        assert_eq!(ctx.peek(), Token { 
+            tty: TokenType::Number,
+            lex: "34"
+        });
+    }
+
+    #[test]
+    fn test_peek_empty() {
+        let source = "blah";
+        let tokens = vec![
+        ];
+        let ctx = ParsingContext::from(source, tokens); 
+
+        assert_eq!(ctx.peek(), Token { 
+            tty: TokenType::Eof,
+            lex: ""
+        });
+    }
+
+}
+
+#[cfg(test)]
+mod end_user_tests {
     use super::*;
 
     #[test]
@@ -211,6 +300,29 @@ mod tests {
             lex: Cow::from(source)
         });
 
+        let actual = loads(source).expect("this should parse");
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn parse_object_simple() {
+        let source = "{ \"foo\": \"bar\" }";
+        let expected = JsonData(JsonDataInner {
+            ty: JsonDataType::Object {
+                entries: vec![
+                    (
+                        JsonKey { lex: Cow::from("\"foo\"") },
+                        JsonData(JsonDataInner {
+                            ty: JsonDataType::Str,
+                            lex: Cow::from("\"bar\"")
+                        })
+                    )
+                ]
+            },
+            lex: Cow::from(source)
+        });
+         
         let actual = loads(source).expect("this should parse");
 
         assert_eq!(actual, expected);
